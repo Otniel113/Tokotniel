a4d25cffafcaf3fdb0f2a6bfedc24746037bca7e
Otniel113 <otniel.abiezer.saragih@gmail.com>
2026-01-11 20:12:47 +0700

    fix: Swagger 404 in production

--- src/app.js
+++ src/app.js
@@ -14,6 +14,11 @@ app.use(express.json());
 const swaggerOptions = {
   customCss: '.swagger-ui .topbar { display: none }',
   customSiteTitle: "Tokotniel API Docs",
+  customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
+  customJs: [
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js"
+  ],
   explorer: true
 };
 
